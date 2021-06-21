aa00b7b6b91a7e5510543ca122d82f73529d85c7
Jeniffer Martins <martins.jeniffer@usp.br>
2021-06-20 21:37:12 -0300

    Iniciando projeto com Firebase

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button text="botão1" />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
